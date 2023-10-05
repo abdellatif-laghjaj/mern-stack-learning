1a6bed94eaa0bf6b778b16cd80419e805949a91b
abdellatif-laghjaj <abdelatiflaghjaj@gmail.com>
2023-10-05 02:04:36 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
