ce24cdf923b729a0361438d6ab20904d60a36249
abdellatif-laghjaj <abdelatiflaghjaj@gmail.com>
2023-10-05 02:06:41 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBook from "./pages/CreateBook";
+import UpdateBook from "./pages/UpdateBook";
+import DeleteBook from "./pages/DeleteBook";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/" element={Home} />
+      <Route path="/books" element={CreateBook} />
+      <Route path="/books/:id" element={UpdateBook} />
+      <Route path="/books/:id" element={DeleteBook} />
     </Routes>
   );
 }
